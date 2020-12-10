a5268d34b89f7cd06edb18163aab31a307af6cb0
Lebo <kebithil@gmail.com>
2020-12-10 19:42:55 +0200

    removed for loop

--- Task4.py
+++ Task4.py
@@ -2,11 +2,10 @@ def check_three(x, y):
 
   sum = x + y 
 
-  for i in str(sum):
-    if str(3) in str(sum):
-      z = True
-    else:
-      z = False
+  if str(3) in str(sum):
+    z = True
+  else:
+    z = False
 
   if((x == 3 or y == 3) and z == True):
     return True
